d13af9e12aed807f318104ef5b5ea8a02d8b3d6a
Mr-rabbit-xd <bihh5539@gmail.com>
2025-07-15 02:26:31 +0530

    Update key.js

--- models/key.js
+++ models/key.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const keySchema = new mongoose.Schema({
-  key: { type: String, unique: true, required: true },
-  used: { type: Boolean, default: false },
-  usedBy: { type: String, default: null },
-  createdBy: { type: String, default: 'admin' },
-  createdAt: { type: Date, default: Date.now }
+  key: { type: String, required: true, unique: true },
+  used: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Key', keySchema);
+module.exports = mongoose.model("Key", keySchema);
