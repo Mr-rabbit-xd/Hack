962c7a49eb3636658c4aaab14eeba4c74a4d087f
Mr-rabbit-xd <bihh5539@gmail.com>
2025-07-15 02:27:15 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  privateKey: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
